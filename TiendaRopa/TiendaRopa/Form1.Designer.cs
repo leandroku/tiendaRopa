35caac82f4b419351f138b356508c8141c5d0bfd
leandroku <87959503+leandroku@users.noreply.github.com>
2021-10-07 22:20:17 -0500

    mejora del entorno visual

--- TiendaRopa/TiendaRopa/Form1.Designer.cs
+++ TiendaRopa/TiendaRopa/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace TiendaRopa
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -41,9 +42,6 @@ namespace TiendaRopa
             this.cboTipo = new System.Windows.Forms.ComboBox();
             this.cboTalla = new System.Windows.Forms.ComboBox();
             this.btNuevaPrenda = new System.Windows.Forms.Button();
-            this.btAgregar = new System.Windows.Forms.Button();
-            this.btActualizar = new System.Windows.Forms.Button();
-            this.btEliminar = new System.Windows.Forms.Button();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
             this.label6 = new System.Windows.Forms.Label();
@@ -57,45 +55,60 @@ namespace TiendaRopa
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
             this.label1.Location = new System.Drawing.Point(20, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 17);
+            this.label1.Size = new System.Drawing.Size(118, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tipo de Prenda";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
             this.label2.Location = new System.Drawing.Point(157, 109);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 17);
+            this.label2.Size = new System.Drawing.Size(43, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Talla";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
             this.label3.Location = new System.Drawing.Point(157, 166);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.Size = new System.Drawing.Size(56, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "Precio";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Window;
             this.label4.Location = new System.Drawing.Point(20, 101);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(47, 17);
+            this.label4.Size = new System.Drawing.Size(54, 17);
             this.label4.TabIndex = 3;
             this.label4.Text = "Marca";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Window;
             this.label5.Location = new System.Drawing.Point(20, 166);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(41, 17);
+            this.label5.Size = new System.Drawing.Size(48, 17);
             this.label5.TabIndex = 4;
             this.label5.Text = "Color";
             // 
@@ -149,40 +162,23 @@ namespace TiendaRopa
             // 
             // btNuevaPrenda
             // 
-            this.btNuevaPrenda.Location = new System.Drawing.Point(336, 65);
+            this.btNuevaPrenda.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btNuevaPrenda.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btNuevaPrenda.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btNuevaPrenda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btNuevaPrenda.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btNuevaPrenda.ForeColor = System.Drawing.SystemColors.Window;
+            this.errorProvider1.SetIconAlignment(this.btNuevaPrenda, System.Windows.Forms.ErrorIconAlignment.MiddleLeft);
+            this.errorProvider2.SetIconAlignment(this.btNuevaPrenda, System.Windows.Forms.ErrorIconAlignment.MiddleLeft);
+            this.btNuevaPrenda.Image = ((System.Drawing.Image)(resources.GetObject("btNuevaPrenda.Image")));
+            this.btNuevaPrenda.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btNuevaPrenda.Location = new System.Drawing.Point(358, 65);
             this.btNuevaPrenda.Name = "btNuevaPrenda";
-            this.btNuevaPrenda.Size = new System.Drawing.Size(75, 23);
+            this.btNuevaPrenda.Size = new System.Drawing.Size(244, 86);
             this.btNuevaPrenda.TabIndex = 11;
-            this.btNuevaPrenda.Text = "Nuevo";
-            this.btNuevaPrenda.UseVisualStyleBackColor = true;
+            this.btNuevaPrenda.Text = "            Nuevo";
+            this.btNuevaPrenda.UseVisualStyleBackColor = false;
             this.btNuevaPrenda.Click += new System.EventHandler(this.btNuevo_Click);
-            // 
-            // btAgregar
-            // 
-            this.btAgregar.Location = new System.Drawing.Point(336, 109);
-            this.btAgregar.Name = "btAgregar";
-            this.btAgregar.Size = new System.Drawing.Size(75, 23);
-            this.btAgregar.TabIndex = 12;
-            this.btAgregar.Text = "Agregar";
-            this.btAgregar.UseVisualStyleBackColor = true;
-            // 
-            // btActualizar
-            // 
-            this.btActualizar.Location = new System.Drawing.Point(436, 65);
-            this.btActualizar.Name = "btActualizar";
-            this.btActualizar.Size = new System.Drawing.Size(75, 23);
-            this.btActualizar.TabIndex = 13;
-            this.btActualizar.Text = "Actualizar";
-            this.btActualizar.UseVisualStyleBackColor = true;
-            // 
-            // btEliminar
-            // 
-            this.btEliminar.Location = new System.Drawing.Point(436, 109);
-            this.btEliminar.Name = "btEliminar";
-            this.btEliminar.Size = new System.Drawing.Size(75, 23);
-            this.btEliminar.TabIndex = 14;
-            this.btEliminar.Text = "Eliminar";
-            this.btEliminar.UseVisualStyleBackColor = true;
             // 
             // errorProvider1
             // 
@@ -195,9 +191,12 @@ namespace TiendaRopa
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(355, 186);
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Window;
+            this.label6.Location = new System.Drawing.Point(385, 186);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(135, 17);
+            this.label6.Size = new System.Drawing.Size(191, 22);
             this.label6.TabIndex = 15;
             this.label6.Text = "Numero de Prendas";
             // 
@@ -213,6 +212,7 @@ namespace TiendaRopa
             // dgvLista
             // 
             this.dgvLista.AllowUserToOrderColumns = true;
+            this.dgvLista.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
             this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvLista.Location = new System.Drawing.Point(23, 252);
             this.dgvLista.Name = "dgvLista";
@@ -225,13 +225,13 @@ namespace TiendaRopa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(723, 450);
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(685, 419);
             this.Controls.Add(this.dgvLista);
             this.Controls.Add(this.lblNumeroPrendas);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.btEliminar);
-            this.Controls.Add(this.btActualizar);
-            this.Controls.Add(this.btAgregar);
             this.Controls.Add(this.btNuevaPrenda);
             this.Controls.Add(this.cboTalla);
             this.Controls.Add(this.cboTipo);
@@ -243,6 +243,8 @@ namespace TiendaRopa
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -267,9 +269,6 @@ namespace TiendaRopa
         private System.Windows.Forms.ComboBox cboTipo;
         private System.Windows.Forms.ComboBox cboTalla;
         private System.Windows.Forms.Button btNuevaPrenda;
-        private System.Windows.Forms.Button btAgregar;
-        private System.Windows.Forms.Button btActualizar;
-        private System.Windows.Forms.Button btEliminar;
         private System.Windows.Forms.ErrorProvider errorProvider1;
         private System.Windows.Forms.ErrorProvider errorProvider2;
         private System.Windows.Forms.Label lblNumeroPrendas;
